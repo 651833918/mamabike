e890d27ed62d02491af0e654d48b3ff5ae8347fc
Xue <651833918@qq.com>
2017-10-28 13:17:35 +0800

    对接七牛云存储

--- src/main/java/cn/powerr/mamabike/common/constant/Constants.java
+++ src/main/java/cn/powerr/mamabike/common/constant/Constants.java
@@ -17,11 +17,19 @@ public class Constants {
     public static final String REQUEST_VERSION_KEY = "version";
     public static final String REQUEST_TOKEN_KEY = "user-token";
 
-    /**秒滴认证开始**/
+    /**
+     * 秒滴认证开始
+     **/
     public static final String MDSMS_AUTH_TOKEN = "735c7a5090074c42b217bd4fc65d6425";
     public static final String MDSMS_ACCOUNT_SID = "5f46ae51f1574dc59d5880203e4e8f30";
     public static final String MDSMS_REST_URL = "https://api.miaodiyun.com/20150822";
     public static final String MDSMS_VERCODE_TPLID = "93565687";
     /**秒滴认证结束**/
 
+    /**七牛云存储key**/
+    public static final String QINIU_ACCESS_KEY = "uwNmKMge2w4EUXL3P7FSRF2eHkhmYSCx9AGOQQIT";
+    public static final String QINIU_SECRET_KEY = "Pgrs2LIx6tWqakjnnJrSvtycjJlYKBeVLYnfoUug";
+    public static final String QINIU_HEAD_IMG_BUCKET_NAME = "mamabike";
+    public static final String QINIU_HEAD_IMG_BUCKET_URL = "oyihp3q7n.bkt.clouddn.com";
+    /**七牛云存储key结束**/
 }
